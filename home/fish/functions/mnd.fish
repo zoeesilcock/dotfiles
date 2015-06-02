94716d841e88cc7b5ab0e2c995ed3a4732b2a9c0
Zoee Silcock <mrzoee@gmail.com>
2015-06-02 22:40:46 +0200

    Only include MND commands if they exist

--- home/fish/functions/mnd.fish
+++ home/fish/functions/mnd.fish
@@ -1,2 +1,7 @@
-set --universal fish_user_paths "~/Projects/mndx-dev/bin" $fish_user_paths
-alias mnd "bash ~/Projects/mnd/bin/mnd"
+if test -d "~/Projects/mndx-dev/bin"
+  set --universal fish_user_paths "~/Projects/mndx-dev/bin" $fish_user_paths
+end
+
+if test -d "~/Projects/mndx/bin"
+  alias mnd "bash ~/Projects/mnd/bin/mnd"
+end
