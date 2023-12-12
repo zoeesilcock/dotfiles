faa153721c670d666cb29e946d8c93ec2b23d131
Zoee Silcock <mrzoee@gmail.com>
2023-12-12 10:05:54 +0100

    Default to git files in telescope

--- home/.config/nvim/after/plugin/telescope.lua
+++ home/.config/nvim/after/plugin/telescope.lua
@@ -8,7 +8,7 @@ require('telescope').setup{
   }
 }
 
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep >") })
 end)
