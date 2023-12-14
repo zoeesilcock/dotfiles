48ffacf00c057594fc386fb004d8fc4219269bf5
Zoee Silcock <mrzoee@gmail.com>
2023-12-14 07:55:49 +0100

    Configure LSP in neovim

--- home/.config/nvim/after/plugin/lsp.lua
+++ home/.config/nvim/after/plugin/lsp.lua
@@ -1,2 +1,25 @@
-local lsp = require("lsp-zero")
-lsp.preset("recommended")
+local lsp_zero = require("lsp-zero")
+lsp_zero.preset("recommended")
+
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+lsp_zero.setup()
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'tsserver', 'eslint', 'lua_ls'},
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+  })
+})
--- home/.config/nvim/lua/zoee/plugins.lua
+++ home/.config/nvim/lua/zoee/plugins.lua
@@ -35,11 +35,15 @@ require("lazy").setup({
   {"mbbill/undotree"},
   {"numToStr/Comment.nvim", opts = {}},
   {"tpope/vim-fugitive"},
-  {"williamboman/mason.nvim"},
-  {"williamboman/mason-lspconfig.nvim"},
+
+  -- LSP and completion
   {"VonHeikemen/lsp-zero.nvim", branch = "v3.x"},
   {"neovim/nvim-lspconfig"},
   {"hrsh7th/cmp-nvim-lsp"},
   {"hrsh7th/nvim-cmp"},
   {"L3MON4D3/LuaSnip"},
+
+  -- LSP installation
+  {"williamboman/mason.nvim"},
+  {"williamboman/mason-lspconfig.nvim"},
 })
