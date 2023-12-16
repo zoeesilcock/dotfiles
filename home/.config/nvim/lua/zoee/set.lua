14954c12e01f86f1a28ecf22ebde9c63935bf522
Zoee Silcock <mrzoee@gmail.com>
2023-12-16 15:28:24 +0100

    Move nvim backups out of the current directory

--- home/.config/nvim/lua/zoee/set.lua
+++ home/.config/nvim/lua/zoee/set.lua
@@ -11,8 +11,8 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
-vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.backupdir = os.getenv("HOME").. "/.vim/backupdir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = true
