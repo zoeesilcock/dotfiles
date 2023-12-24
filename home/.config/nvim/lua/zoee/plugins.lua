cd9afc1d2c6a123f1699d50f7548eb9c3ac5f71a
Zoee Silcock <mrzoee@gmail.com>
2023-12-24 13:46:54 +0100

    Update neovim plugins

--- home/.config/nvim/lua/zoee/plugins.lua
+++ home/.config/nvim/lua/zoee/plugins.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.4",
+    branch = "0.1.x",
     dependencies = { "nvim-lua/plenary.nvim" }
   },
   {
