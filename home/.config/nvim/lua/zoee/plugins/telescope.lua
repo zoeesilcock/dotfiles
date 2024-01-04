f723e2f63bde9c78ac9d32e7d3cf45fcf1c4d688
Zoee Silcock <mrzoee@gmail.com>
2024-01-04 19:03:49 +0100

    Add a native fuzzy finder for telescope

--- home/.config/nvim/lua/zoee/plugins/telescope.lua
+++ home/.config/nvim/lua/zoee/plugins/telescope.lua
@@ -17,5 +17,12 @@ return {
         }
       }
     end
+  },
+  {"nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    dependencies = {"nvim-telescope/telescope.nvim"},
+    config = function()
+      require('telescope').load_extension('fzf')
+    end
   }
 }
