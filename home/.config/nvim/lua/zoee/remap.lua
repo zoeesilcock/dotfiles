241a61b75c8c40fea632467c9540fbcefe21579b
Zoee Silcock <mrzoee@gmail.com>
2024-01-05 13:00:09 +0100

    Remove some unused neovim mappings

--- home/.config/nvim/lua/zoee/remap.lua
+++ home/.config/nvim/lua/zoee/remap.lua
@@ -32,12 +32,6 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
--- Quick fix navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
 -- Replace current word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
