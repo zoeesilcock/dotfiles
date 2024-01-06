7743c86e5dc08a883777a303482d1696e2c389b7
Zoee Silcock <mrzoee@gmail.com>
2024-01-06 16:20:01 +0100

    Replace lsp-zero with manual config

--- home/.config/nvim/lua/zoee/plugins/lsp.lua
+++ home/.config/nvim/lua/zoee/plugins/lsp.lua
@@ -4,28 +4,21 @@ return {
       require("mason").setup({})
     end,
   },
-  {"VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    dependencies = {"L3MON4D3/LuaSnip"},
-    config = function()
-      local lsp_zero = require("lsp-zero")
-      lsp_zero.preset("recommended")
-
-      lsp_zero.on_attach(function(client, bufnr)
-        lsp_zero.default_keymaps({buffer = bufnr})
-      end)
-      lsp_zero.setup()
-    end,
-  },
   {"williamboman/mason-lspconfig.nvim",
     dependencies = {
       "neovim/nvim-lspconfig",
       "williamboman/mason.nvim",
-      "VonHeikemen/lsp-zero.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "folke/trouble.nvim",
     },
     config = function()
-      local lsp_zero = require("lsp-zero")
-      lsp_zero.extend_lspconfig();
+      local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local default_setup = function(server)
+        require("lspconfig")[server].setup({
+          capabilities = lsp_capabilities,
+        })
+      end
 
       require("mason-lspconfig").setup({
         ensure_installed = {
@@ -39,7 +32,7 @@ return {
           "taplo",
         },
         handlers = {
-          lsp_zero.default_setup,
+          default_setup,
           lua_ls = function()
             settings = {
               Lua = {
