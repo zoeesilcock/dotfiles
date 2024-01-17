4b52877a705e2eb4861ed92003a2c6c6f7985c51
Zoee Silcock <mrzoee@gmail.com>
2024-01-17 18:37:18 +0100

    Use double quotes in nvim config

--- home/.config/nvim/lua/zoee/plugins/lsp.lua
+++ home/.config/nvim/lua/zoee/plugins/lsp.lua
@@ -50,29 +50,29 @@ return {
         },
       })
 
-      vim.api.nvim_create_autocmd('LspAttach', {
-        desc = 'LSP actions',
+      vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "LSP actions",
         callback = function(event)
           local opts = {buffer = event.buf}
 
           -- these will be buffer-local keybindings
           -- because they only work if you have an active language server
-          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-          vim.keymap.set('n', 'S', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+          vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+          vim.keymap.set("n", "S", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
 
-          vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-          vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-          vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+          vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+          vim.keymap.set({"n", "x"}, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+          vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 
-          vim.keymap.set('n', 'gi', '<cmd>TroubleToggle lsp_implementations<cr>', opts)
-          vim.keymap.set('n', 'gr', '<cmd>TroubleToggle lsp_references<cr>', opts)
-          vim.keymap.set('n', 'gd', '<cmd>TroubleToggle lsp_definitions<cr>', opts)
-          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-          vim.keymap.set('n', 'go', '<cmd>TroubleToggle lsp_type_definitions<cr>', opts)
+          vim.keymap.set("n", "gi", "<cmd>TroubleToggle lsp_implementations<cr>", opts)
+          vim.keymap.set("n", "gr", "<cmd>TroubleToggle lsp_references<cr>", opts)
+          vim.keymap.set("n", "gd", "<cmd>TroubleToggle lsp_definitions<cr>", opts)
+          vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+          vim.keymap.set("n", "go", "<cmd>TroubleToggle lsp_type_definitions<cr>", opts)
 
           -- Diagnostics
           vim.diagnostic.config({ virtual_text = false })
-          vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+          vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
         end
       })
     end,
--- home/.config/nvim/lua/zoee/plugins/telescope.lua
+++ home/.config/nvim/lua/zoee/plugins/telescope.lua
@@ -29,7 +29,7 @@ return {
     build = "make",
     dependencies = {"nvim-telescope/telescope.nvim"},
     config = function()
-      require('telescope').load_extension('fzf')
+      require("telescope").load_extension("fzf")
     end
   }
 }
