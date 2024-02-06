4ff3e74edfafb77e61ab40d53eefd1e01e5e904f
Zoee Silcock <mrzoee@gmail.com>
2024-02-06 15:56:13 +0100

    Add keymap for jumping between trouble entries without switching to the trouble window

--- home/.config/nvim/lua/zoee/plugins/trouble.lua
+++ home/.config/nvim/lua/zoee/plugins/trouble.lua
@@ -7,6 +7,8 @@ return {
       {"<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>"},
       {"<leader>xq", "<cmd>TroubleToggle quickfix<CR>"},
       {"<leader>xl", "<cmd>TroubleToggle loclist<CR>"},
+      {"<leader>xn", function() require("trouble").next({ skip_groups = true, jump = true }) end, desc = "next trouble"},
+      {"<leader>xp", function() require("trouble").previous({ skip_groups = true, jump = true }) end, desc = "previous trouble"},
     }
   },
 }
