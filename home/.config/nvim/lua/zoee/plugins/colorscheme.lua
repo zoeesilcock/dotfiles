349f09e880470ca42ec1faf2c621a90ebc52dddf
Zoee Silcock <mrzoee@gmail.com>
2024-02-23 12:19:42 +0100

    Switch to the catppuccin-mocha colorscheme

--- home/.config/nvim/lua/zoee/plugins/colorscheme.lua
+++ home/.config/nvim/lua/zoee/plugins/colorscheme.lua
@@ -1,10 +1,22 @@
 return {
-  {"chriskempson/base16-vim",
+  -- {"chriskempson/base16-vim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.opt.background = "dark"
+  --     vim.cmd.colorscheme("base16-tomorrow-night")
+  --   end
+  -- }
+  {"catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.opt.background = "dark"
-      vim.cmd.colorscheme("base16-tomorrow-night")
-    end
+      require("catppuccin").setup({
+        flavour = "mocha",
+        no_italic = true,
+      })
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
   }
 }
