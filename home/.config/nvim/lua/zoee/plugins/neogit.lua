42f6ab80446f1df96c67fd369a0842a5dba192a5
Zoee Silcock <mrzoee@gmail.com>
2024-04-15 12:03:05 +0200

    Add keybinds for the diffview.nvim and gitsigns.nvim plugins

--- home/.config/nvim/lua/zoee/plugins/neogit.lua
+++ home/.config/nvim/lua/zoee/plugins/neogit.lua
@@ -6,12 +6,45 @@ return {
     },
     keys = {
       { "<leader>gs", "<cmd>Neogit kind=split_above<CR>", desc = "Launch neogit" },
+      { "<leader>gd", "<cmd>DiffviewFileHistory %<CR>", mode = "n", desc = "Launch diffview for current file" },
+      { "<leader>gd", "<cmd>'<,'>DiffviewFileHistory<CR>", mode = "v", desc = "Launch diffview for current line" },
     },
-    config = true
+    config = function()
+      require("neogit").setup()
+      require("diffview").setup()
+    end,
   },
   {"lewis6991/gitsigns.nvim",
+    lazy = false,
+    keys = {
+      {
+        "<leader>gb",
+        function()
+          require("gitsigns").blame_line{full=true}
+        end,
+        desc = "Show blame for current line",
+      },
+      {
+        "<leader>gn",
+        function()
+          if not vim.wo.diff then
+            require("gitsigns").nav_hunk("next")
+          end
+        end,
+        desc = "Go to next hunk",
+      },
+      {
+        "<leader>gp",
+        function()
+          if not vim.wo.diff then
+            require("gitsigns").nav_hunk("prev")
+          end
+        end,
+        desc = "Go to previous hunk",
+      },
+    },
     config = function()
-      require('gitsigns').setup()
+      require("gitsigns").setup()
     end
   }
 }
