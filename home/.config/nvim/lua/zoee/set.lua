ce3b4c99a974cf501289e54fc5def429c10a72ed
Zoee Silcock <mrzoee@gmail.com>
2024-05-29 07:13:39 +0200

    Use PowerShell as our shell in nvim on Windows

--- home/.config/nvim/lua/zoee/set.lua
+++ home/.config/nvim/lua/zoee/set.lua
@@ -52,6 +52,23 @@ vim.g.rustfmt_autosave = 1
 -- Zig
 vim.g.zig_fmt_autosave = 0
 
+-- Windows specific
+if (vim.loop.os_uname().sysname == "Windows_NT") then
+  local powershell_options = {
+    shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
+end
+
 -- Neovide specific
 if vim.g.neovide then
   vim.g.neovide_scroll_animation_length = 0.1
