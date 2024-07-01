13432c2a164f9d7fbbb3ab6b0f04486f0a249646
Zoee Silcock <mrzoee@gmail.com>
2024-07-01 11:37:55 +0200

    Allow running zig shortcuts from insert mode

--- home/.config/nvim/after/ftplugin/zig.lua
+++ home/.config/nvim/after/ftplugin/zig.lua
@@ -1,10 +1,14 @@
 vim.keymap.set("n", "<S-F5>", "<cmd>call zig#fmt#Format()<CR>")
+vim.keymap.set("i", "<S-F5>", "<c-c><cmd>call zig#fmt#Format()<CR>")
 vim.keymap.set("n", "<F5>", "<cmd>wa<CR><cmd>TermExec cmd=\"clear ; zig build\"<CR>")
+vim.keymap.set("i", "<F5>", "<c-c><cmd>wa<CR><cmd>TermExec cmd=\"clear ; zig build\"<CR>")
 
 if string.match(vim.fn.getcwd(), "handmade%-zig") then
     if (vim.loop.os_uname().sysname == "Windows_NT") then
         vim.keymap.set("n", "<F6>", "<cmd>TermExec cmd=\"zig build ; Start-Process -NoNewWindow -FilePath ./zig-out/bin/handmade-zig.exe -WorkingDirectory ./data\"<CR>")
+        vim.keymap.set("i", "<F6>", "<c-c><cmd>TermExec cmd=\"zig build ; Start-Process -NoNewWindow -FilePath ./zig-out/bin/handmade-zig.exe -WorkingDirectory ./data\"<CR>")
     else
         vim.keymap.set("n", "<F6>", "<cmd>TermExec cmd=\"zig build && ./zig-out/bin/handmade-zig -WorkingDirectory ./data\"<CR>")
+        vim.keymap.set("i", "<F6>", "<c-c><cmd>TermExec cmd=\"zig build && ./zig-out/bin/handmade-zig -WorkingDirectory ./data\"<CR>")
     end
 end
