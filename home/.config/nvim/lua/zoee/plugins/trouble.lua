909cad4ecd5d001b35baca07288de7f08e4ebcc5
Zoee Silcock <mrzoee@gmail.com>
2024-07-01 14:34:24 +0200

    Fix the mappings for navigating diagnostics in trouble

--- home/.config/nvim/lua/zoee/plugins/trouble.lua
+++ home/.config/nvim/lua/zoee/plugins/trouble.lua
@@ -10,8 +10,8 @@ return {
       {"<leader>xx", "<cmd>Trouble diagnostics toggle<CR>"},
       {"<leader>xq", "<cmd>Trouble qflist toggle<CR>"},
       {"<leader>xl", "<cmd>Trouble loclist toggle<CR>"},
-      {"<leader>xn", function() require("trouble").next({ skip_groups = true, jump = true }) end, desc = "next trouble"},
-      {"<leader>xp", function() require("trouble").previous({ skip_groups = true, jump = true }) end, desc = "previous trouble"},
+      {"<leader>xn", function() require("trouble").next({ mode = "diagnostics", skip_groups = true, jump = true }) end, desc = "next trouble"},
+      {"<leader>xp", function() require("trouble").previous({ mode = "diagnostics", skip_groups = true, jump = true }) end, desc = "previous trouble"},
     }
   },
 }
