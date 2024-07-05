196214d5f124b5d9524e6f9065fa74eee07971e3
Zoee Silcock <mrzoee@gmail.com>
2024-07-05 13:12:59 +0200

    Update the trouble mappings to work with multiple trouble types

--- home/.config/nvim/lua/zoee/plugins/trouble.lua
+++ home/.config/nvim/lua/zoee/plugins/trouble.lua
@@ -1,3 +1,16 @@
+function getTroubleMode()
+  local trouble = require("trouble")
+  local mode = "diagnostics"
+
+  if (trouble.is_open("telescope")) then
+    mode = "telescope"
+  elseif (trouble.is_open("lsp_references")) then
+    mode = "lsp_references"
+  end
+
+  return mode
+end
+
 return {
   {"folke/trouble.nvim",
     dependencies = {"nvim-tree/nvim-web-devicons"},
@@ -10,8 +23,8 @@ return {
       {"<leader>xx", "<cmd>Trouble diagnostics toggle<CR>"},
       {"<leader>xq", "<cmd>Trouble qflist toggle<CR>"},
       {"<leader>xl", "<cmd>Trouble loclist toggle<CR>"},
-      {"<leader>xn", function() require("trouble").next({ mode = "diagnostics", skip_groups = true, jump = true }) end, desc = "next trouble"},
-      {"<leader>xp", function() require("trouble").previous({ mode = "diagnostics", skip_groups = true, jump = true }) end, desc = "previous trouble"},
+      {"<leader>xn", function() require("trouble").next({ mode = getTroubleMode(), skip_groups = true, jump = true }) end, desc = "next trouble"},
+      {"<leader>xp", function() require("trouble").prev({ mode = getTroubleMode(), skip_groups = true, jump = true }) end, desc = "previous trouble"},
     }
   },
 }
