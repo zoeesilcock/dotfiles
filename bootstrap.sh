e8b9b6162a4da7fac935b90f545b9b616122c32e
Zoee Silcock <mrzoee@gmail.com>
2024-09-09 17:16:34 +0200

    Fix alacritty configuration on Mac

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,6 +48,7 @@ done
 
 symlink "$(pwd)/home/.zsh_prompt" "$HOME/.oh-my-zsh/custom/themes/zoees.zsh-theme"
 symlink "$(pwd)/home/.config/nvim" "$HOME/.config/nvim"
+symlink "$(pwd)/home/.config/alacritty" "$HOME/.config/alacritty"
 symlink "$(pwd)/home/.config/nvim-vscode" "$HOME/.config/nvim-vscode"
 
 echo "Installing VIM plugins"
