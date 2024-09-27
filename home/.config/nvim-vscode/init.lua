d9472a4d8a8dc17c561515a010fbe003d3f172fc
Zoee Silcock <mrzoee@gmail.com>
2024-09-27 11:57:40 +0200

    Update mappings used in vscode neovim plugin

--- home/.config/nvim-vscode/init.lua
+++ home/.config/nvim-vscode/init.lua
@@ -29,6 +29,28 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 -- Clear search highlight
 vim.keymap.set("n", "<leader>n", ":noh<CR>")
 
+-- Allow typing distant characters from the home row.
+vim.keymap.set("i", "ä ", "ä ", { desc = "ä space" })
+vim.keymap.set("i", "ä<CR>", "ä<CR>", { desc = "ä enter" })
+vim.keymap.set("i", "ää", "ä<Esc>", { desc = "Lone ä" })
+
+vim.keymap.set("i", "äa", "&", { desc = "&" })
+vim.keymap.set("i", "äq", "/", { desc = "/" })
+
+vim.keymap.set("i", "äw", "''<Esc>i", { desc = "single quotes" })
+vim.keymap.set("i", "äs", '""<Esc>i', { desc = "double quotes" })
+
+vim.keymap.set("i", "äd", "()<Esc>i", { desc = "parens" })
+vim.keymap.set("i", "äe", "[]<Esc>i", { desc = "brackets" })
+
+vim.keymap.set("i", "äf", "{}<Esc>i", { desc = "curlies" })
+vim.keymap.set("i", "är", "||<Esc>i", { desc = "pipes" })
+
+vim.keymap.set("i", "äj", "@", { desc = "@" })
+
+-- Since ctrl-h is backspace in insert mode, this makes ctrl-l delete.
+vim.keymap.set("i", "<c-l>", "<del>", { desc = "delete" })
+
 
 -- Options
 vim.opt.relativenumber = true
