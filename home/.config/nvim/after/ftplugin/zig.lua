c02ae9fa8092f19cfa7af5d2fa889011f86685a4
Zoee Silcock <mrzoee@gmail.com>
2025-12-08 16:31:55 +0100

    Allow F6 to build and run from any zig file

--- home/.config/nvim/after/ftplugin/zig.lua
+++ home/.config/nvim/after/ftplugin/zig.lua
@@ -1,15 +1,12 @@
-vim.keymap.set("n", "<S-F5>", "<cmd>!zig fmt %<CR><cmd>:e<CR><cmd>LspRestart<CR>")
-vim.keymap.set("i", "<S-F5>", "<c-c><cmd>!zig fmt %<CR><cmd>:e<CR><cmd>LspRestart<CR>")
-
 vim.keymap.set("n", "<F5>", "<cmd>wa<CR><cmd>TermExec cmd=\"clear ; zig build\"<CR>")
 vim.keymap.set("i", "<F5>", "<c-c><cmd>wa<CR><cmd>TermExec cmd=\"clear ; zig build\"<CR>")
+
+vim.keymap.set("n", "<F6>", "<cmd>TermExec cmd=\"clear && zig build run\"<CR>")
+vim.keymap.set("i", "<F6>", "<c-c><cmd>TermExec cmd=\"clear && zig build run\"<CR>")
 
 if string.match(vim.fn.getcwd(), "handmade%-zig") then
     if (vim.loop.os_uname().sysname == "Windows_NT") then
         vim.keymap.set("n", "<F6>", "<cmd>TermExec cmd=\"clear ; zig build ; Start-Process -NoNewWindow -FilePath ./zig-out/bin/handmade-zig.exe -WorkingDirectory ./data\"<CR>")
         vim.keymap.set("i", "<F6>", "<c-c><cmd>TermExec cmd=\"clear ; zig build ; Start-Process -NoNewWindow -FilePath ./zig-out/bin/handmade-zig.exe -WorkingDirectory ./data\"<CR>")
-    else
-        vim.keymap.set("n", "<F6>", "<cmd>TermExec cmd=\"clear && zig build run\"<CR>")
-        vim.keymap.set("i", "<F6>", "<c-c><cmd>TermExec cmd=\"clear && zig build run\"<CR>")
     end
 end
