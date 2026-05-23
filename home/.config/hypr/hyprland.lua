a8809fd2de7acff63c29e6aff8520551f4e5918b
Zoee Silcock <mrzoee@gmail.com>
2026-05-23 14:37:42 +0200

    Disable the silly hyprland quotes shown on the desktop

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -74,5 +74,6 @@ hl.config({
   misc = {
     force_default_wallpaper = 0,
     disable_hyprland_logo = true,
+    disable_splash_rendering = true,
   },
 })
--- home/.config/hypr/window_rules.lua
+++ home/.config/hypr/window_rules.lua
@@ -1,7 +1,7 @@
 --
 -- Global.
 --
---
+
 hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
 hl.workspace_rule({ workspace = "f[1]",   gaps_out = 0, gaps_in = 0 })
 hl.window_rule({
